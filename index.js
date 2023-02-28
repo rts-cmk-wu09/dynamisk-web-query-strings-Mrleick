c051100df744fb81b663cbf66090d1545ca39413
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-02-28 10:32:55 +0100

    Added h1 to my destinations section

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const destinations = document.getElementById('destinations');
 fetch("data/destinations.json")
   .then(response => response.json())
   .then(data => {
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Apartments for rent'; // Change the text content to your desired heading
+    destinations.appendChild(h1);
+
     for (let i = 0; i < 8; i++) {
       const article = document.createElement('article');
       article.classList.add('destinations__article');
-      
+
       const img = document.createElement('img');
       img.src = data.destinations[i].image;
       article.appendChild(img);
-      
+
       destinations.appendChild(article);
     }
   });
