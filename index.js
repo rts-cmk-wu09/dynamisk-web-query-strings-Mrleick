a695d871d04d00f1c1837e35cb90f4ea781d8074
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-02-28 12:28:15 +0100

    fixed errors in code index.js

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const destinationList = document.getElementById('destination-list');
-
 // Hent alle destinationer fra destinations.json
-fetch('data/destinations.json')
-  .then(response => response.json())
-  .then(destinations => {
-
+fetch('data/destinations.json').then((response) => {
+  return response.json();
+}).then(data =>{
+  
+    const destinationList = document.getElementById('destination-list');
     // Opret et <li> element for hver destination
-    destinationsArray.forEach(destination => {
+    data.destinations.forEach(destination => {
       const listItem = document.createElement('li');
       listItem.classList.add('destination');
       listItem.dataset.id = destination.id;
       listItem.innerHTML = `
-        <img src="img/${destination.image}" alt="${destination.name}">
-        <h2>${destination.name}</h2>
-        <p>${destination.description}</p>
+        <img src="${destination.image}" alt="${destination.name}">
+        <h2>${destination.destination}</h2>
+        <p>${destination.title}</p>
       `;
       destinationList.appendChild(listItem);
     });
+
+
   });
