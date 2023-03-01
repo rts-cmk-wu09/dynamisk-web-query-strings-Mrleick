339c0e768526a8c24a0735a65974e86050811284
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-03-01 09:23:12 +0100

    Cleaned and added code

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+const destinationList = document.getElementById('destination-list');
+
 // Hent alle destinationer fra destinations.json
 fetch('data/destinations.json').then((response) => {
-  return response.json();
+return response.json();
 }).then(data =>{
   
-    const destinationList = document.getElementById('destination-list');
+    
     // Opret et <li> element for hver destination
     data.destinations.forEach(destination => {
       const listItem = document.createElement('li');
       listItem.classList.add('destination');
-      listItem.dataset.id = destination.id;
       listItem.innerHTML = `
       <a href="destination.html?id=${destination.id}">
-        <img src="${destination.image}" alt="${destination.name}">
+        <img src="./img/${destination.image}" alt="${destination.destination}">
         <h2>${destination.destination}</h2>
         <p>${destination.title}</p></a>
       `;
