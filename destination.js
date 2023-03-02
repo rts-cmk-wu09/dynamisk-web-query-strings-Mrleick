d679c8c130dc9849fe8d4a533918364855e45b72
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-03-02 08:52:45 +0100

    Added/removed some html

--- destination.js
+++ destination.js
@@ -9,7 +9,7 @@ fetch('data/destinations.json').then((response) => {
     const imagecontainer = document.createElement('div');
     imagecontainer.classList.add('imagecontainer');
     imagecontainer.innerHTML = `
-    <img src="img_girl.jpg" alt="">
+    
     `;
     detailview.appendChild(imagecontainer);
 
@@ -18,8 +18,8 @@ fetch('data/destinations.json').then((response) => {
     txtcontainer.innerHTML = `
     <h2 class="txtcontainer__town">KØBENHAVN</h2>
     <h1 class="txtcontainer__title">Penthouse, Solrig med 5 altaner</h1>
-    <p>100% af nylige gæster  har givet indtjekningen en femstjernet vurdering</p>
-    <p>Lejligheden er stor og lys og har 5 mindre altaner og en terrasse. Der er 3 soveværeslder og 2 hemse med senge. Lejligheder ligger på Nørrebro, som er en farverig og livlig del af København. Det er gåafstand til cafe på Balders Plads, Nørrebrogade og 3,3 km til Nørreport St.</p>
+    <p class="txtcontainer__procent">100% af nylige gæster  har givet indtjekningen en femstjernet vurdering</p>
+    <p class="txtcontainer__description">Lejligheden er stor og lys og har 5 mindre altaner og en terrasse. Der er 3 soveværeslder og 2 hemse med senge. Lejligheder ligger på Nørrebro, som er en farverig og livlig del af København. Det er gåafstand til cafe på Balders Plads, Nørrebrogade og 3,3 km til Nørreport St.</p>
     `;
     detailview.appendChild(txtcontainer);
 });
