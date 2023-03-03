1ec811d69b364c8ae91e8d0fab437ece0c873dcd
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-03-03 11:56:44 +0100

    Added more code

--- destination.js
+++ destination.js
@@ -1,25 +1,33 @@
-// Hent alle destinationer fra destinations.json
-fetch('data/destinations.json').then((response) => {
-    return response.json();
-}).then(data => {
+const params = new URLSearchParams(document.location.search);
+const id = params.get("id");
 
-    const detailview = document.getElementById('detailview');
-
-
-    const imagecontainer = document.createElement('div');
-    imagecontainer.classList.add('imagecontainer');
-    imagecontainer.innerHTML = `
+fetch(`./data/${id}.json`)
+  .then((response) => response.json())
+  .then((data) => {
     
-    `;
-    detailview.appendChild(imagecontainer);
+const detailview = document.getElementById('detailview');
 
-    const txtcontainer = document.createElement('div');
-    txtcontainer.classList.add('txtcontainer');
-    txtcontainer.innerHTML = `
-    <h2 class="txtcontainer__town">KØBENHAVN</h2>
-    <h1 class="txtcontainer__title">Penthouse, Solrig med 5 altaner</h1>
-    <p class="txtcontainer__procent">100% af nylige gæster  har givet indtjekningen en femstjernet vurdering</p>
-    <p class="txtcontainer__description">Lejligheden er stor og lys og har 5 mindre altaner og en terrasse. Der er 3 soveværeslder og 2 hemse med senge. Lejligheder ligger på Nørrebro, som er en farverig og livlig del af København. Det er gåafstand til cafe på Balders Plads, Nørrebrogade og 3,3 km til Nørreport St.</p>
-    `;
-    detailview.appendChild(txtcontainer);
-});
+
+const imagecontainer = document.createElement('div');
+imagecontainer.classList.add('imagecontainer');
+imagecontainer.innerHTML = `
+<img class="imagecontainer__img" src="img/${data.image}"></img>
+`;
+detailview.appendChild(imagecontainer);
+
+const txtcontainer = document.createElement('div');
+txtcontainer.classList.add('txtcontainer');
+txtcontainer.innerHTML = `
+<h2 class="txtcontainer__town">${data.destination}</h2>
+<h1 class="txtcontainer__title">${data.title}</h1>
+<p class="txtcontainer__procent">${data.subtitle}</p>
+<p class="txtcontainer__description">${data.text}</p>
+<h3>Faciliteter</h3>
+<ul class="txtcontainer__list">
+${data.facilities.map(facility => `<li>${facility}</li>`).join('')}
+</ul>
+`;
+detailview.appendChild(txtcontainer);
+
+  })
+
