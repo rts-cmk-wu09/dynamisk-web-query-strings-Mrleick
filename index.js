f059a644dc703fcbb85f629cf1e974c2b65ea46e
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-03-03 11:57:01 +0100

    Added class

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ return response.json();
       listItem.classList.add('destination');
       listItem.innerHTML = `
       <a href="destination.html?id=${destination.id}">
-        <img src="./img/${destination.image}" alt="${destination.destination}">
+        <img class="detailview__img" src="./img/${destination.image}" alt="${destination.destination}">
         <h2>${destination.destination}</h2>
         <p>${destination.title}</p></a>
       `;
