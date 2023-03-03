8f10f1658ff20503c1d544b5ef12d6073c226069
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-03-03 14:08:31 +0100

    Added code to like heart

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const destinationList = document.getElementById('destination-list');
 
-// Hent alle destinationer fra destinations.json
-fetch('data/destinations.json').then((response) => {
-return response.json();
-}).then(data =>{
-  
-    
-    // Opret et <li> element for hver destination
+// Min html opsætning
+fetch('data/destinations.json')
+  .then(response => response.json())
+  .then(data => {
     data.destinations.forEach(destination => {
       const listItem = document.createElement('li');
       listItem.classList.add('destination');
       listItem.innerHTML = `
-      <a href="destination.html?id=${destination.id}">
-        <img class="detailview__img" src="./img/${destination.image}" alt="${destination.destination}">
-        <h2>${destination.destination}</h2>
-        <p>${destination.title}</p></a>
+        <i class="fa-regular fa-heart"></i>
+        <a href="destination.html?id=${destination.id}">
+          <img class="destination__img" src="./img/${destination.image}" alt="${destination.destination}">
+          <h2>${destination.destination}</h2>
+          <p>${destination.title}</p>
+        </a>
       `;
+
+      // Mit hjerteikon like
+      const heartLike = listItem.querySelector('.fa-heart');
+      heartLike.addEventListener('click', () => {
+        heartLike.classList.toggle('fa-solid');
+        localStorage.setItem(destination.id, heartLike.classList.contains('fa-solid'));
+      });
+
+      // Sæt hjerteikonets tilstand baseret på localStorage
+      const heartSolid = localStorage.getItem(destination.id);
+      if (heartSolid === 'true') {
+        heartLike.classList.add('fa-solid');
+      }
+
       destinationList.appendChild(listItem);
     });
-
-
   });
+
